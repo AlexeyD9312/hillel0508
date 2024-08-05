3c04a7a87c58ed8a33f7af905d4f51e01e6372f7
User <aviator171513@gmail.com>
2024-08-05 18:00:03 +0300

    homework 2.2

---
+++ Homework2.2.py
@@ -0,0 +1,11 @@
+#number=int(input('Print:'))
+#n1=number//1%10
+#n2=(number//10%10)
+#n3=number//100%10
+#n4=number//1000%10
+#n5=number//10000
+#print(n1,n2,n3,n4,n5)
+
+a=input("Your number:")
+b=(a[::-1])
+print(b)
