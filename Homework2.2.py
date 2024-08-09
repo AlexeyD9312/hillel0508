0f21ab81096cf7c92a5bf1dba03bc02fe4c00afd
User <aviator171513@gmail.com>
2024-08-10 01:03:31 +0300

    homework 2.2

--- Homework2.2.py
+++ Homework2.2.py
@@ -1,11 +1,13 @@
-#number=int(input('Print:'))
-#n1=number//1%10
-#n2=(number//10%10)
-#n3=number//100%10
-#n4=number//1000%10
-#n5=number//10000
-#print(n1,n2,n3,n4,n5)
+number=int(input('Print:'))
+n1=number//1%10
+n2=(number//10%10)
+n3=number//100%10
+n4=number//1000%10
+n5=number//10000
+k1=n1*10000
+k2=n2*1000
+k3=n3*100
+k4=n4*10
+k5=n5*1
+print(k1+k2+k3+k4+k5)
 
-a=input("Your number:")
-b=(a[::-1])
-print(b)
