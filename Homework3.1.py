9bcdcb898338922d61e4b38a30222550bb065625
User <aviator171513@gmail.com>
2024-08-12 01:04:09 +0300

    homework 3.1

--- Homework3.1.py
+++ Homework3.1.py
@@ -3,14 +3,22 @@ d=input('математична дія:')
 y=float(input('число 2:'))
 if '*' in d:
     print('дорівнює',x*y)
-elif '+' in d:
-    print('дорівнює',x + y)
-elif '/' in d:
-    print('дорівнює',x / y)
-elif '-' in d :
-    print('дорівнює',x - y)
 else:
-    print('некоректна математична дія!')
+    if '+' in d:
+        print('дорівнює', x + y)
+    else:
+        if '-' in d:
+            print('дорівнює', x - y)
+        else:
+            if y==0:
+                print("на 0 дiлити не можна")
+            else:
+                if '/' in d:
+                    print('дорівнює', x / y)
+                else:
+                    print('некоректна математична дія!')
+
+
 #у цій программі може обчислюватися лише 2 числа
 
 
