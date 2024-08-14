4e047b71e76370fd08c4cc1dc99e5247d40fda82
User <aviator171513@gmail.com>
2024-08-14 16:29:09 +0300

    homework 4.1(var2)

--- Homework4.1.py
+++ Homework4.1.py
@@ -1,5 +1,10 @@
+list=[4,0,0,8,0,1,87,0,5,-3,0,-5,0,-56,6]
+new_list=[]
+zero_list=[]
+for i in list:
+    if i!=0:
+       new_list.append(i)
+    else:
+        zero_list.append(i)
+print(new_list+zero_list)
 
-list=[4,0,0,8,0,1,87,5,-3,0,-5,-56,0,3]
-list_nozero=[i for i in list if i!=0]
-list_zero=[i for i in list if i==0]
-print(list_nozero+list_zero)
