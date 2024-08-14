6a7105a66e1305d0288323fb3c73bf1d3dbf4dcb
User <aviator171513@gmail.com>
2024-08-14 17:19:36 +0300

    homework 4.3(var2)

--- Homework4.3.py
+++ Homework4.3.py
@@ -1,8 +1,9 @@
 import random
 x=[]
-for i in range(10):
+ran=random.randint(3,8)
+for i in range(ran,13,1):
     x.append(random.randint(0,100))
 print(x)
 print(x[0],x[2],x[-2])
 
-
+lst = [random.randint(0, 9) for _ in range(6)]
