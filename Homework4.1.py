5981975a2c5d7479a1fe1bdf420ed0b4211d6150
User <aviator171513@gmail.com>
2024-08-14 15:17:09 +0300

    homework 4.1new

--- Homework4.1.py
+++ Homework4.1.py
@@ -1,11 +1,5 @@
 
-x=[4,0,0,8,0,1,87,5,-3,0,-5,-56,0,4]
-n =len(x)
-k = 0
-for i in list(range(n)):
-    if x[i]:
-        x[k] = x[i]
-        k += 1
-x[k:n:1] = [0]*(n-k)
-print(x)
-
+list=[4,0,0,8,0,1,87,5,-3,0,-5,-56,0,3]
+list_nozero=[i for i in list if i!=0]
+list_zero=[i for i in list if i==0]
+print(list_nozero+list_zero)
