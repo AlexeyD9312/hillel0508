c89c338adcf07f9836ab68582d1963b207b3ba3d
User <aviator171513@gmail.com>
2024-08-15 23:28:21 +0300

    homework 5.2

--- lessons 5/Homework5.2.py
+++ lessons 5/Homework5.2.py
@@ -1,16 +1,22 @@
-x=float(input('число 1:'))
-d=input('математична дія:')
-y=float(input('число 2:'))
-if y!=0:
-    if '*' in d:
-        print('дорівнює',x*y)
-    elif '+' in d:
-        print('дорівнює', x + y)
-    elif '-' in d:
-        print('дорівнює', x - y)
-    elif '/' in d and y!=0:
-           print('дорівнює', x / y)
+while True:
+    x=float(input('число 1:'))
+    d=input('математична дія:')
+    y=float(input('число 2:'))
+    if y!=0:
+        if '*' in d:
+            print('дорівнює',x*y)
+        elif '+' in d:
+            print('дорівнює', x + y)
+        elif '-' in d:
+            print('дорівнює', x - y)
+        elif '/' in d and y!=0:
+               print('дорівнює', x / y)
+        else:
+               print("некоректна дія")
     else:
-           print("некоректна дія")
-else:
-    print('not dilit na 0')
+        print('not dilit na 0')
+    reset = input ("Запустить калькулятор заново: " )
+    if reset == 'y':
+        continue
+    else:
+        break
