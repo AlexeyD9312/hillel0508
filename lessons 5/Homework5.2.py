25cb96169214c413e8b0fb981aafc1bc12a3c736
User <aviator171513@gmail.com>
2024-08-15 21:35:28 +0300

    homework 5.1

---
+++ lessons 5/Homework5.2.py
@@ -0,0 +1,16 @@
+x=float(input('число 1:'))
+d=input('математична дія:')
+y=float(input('число 2:'))
+if y!=0:
+    if '*' in d:
+        print('дорівнює',x*y)
+    elif '+' in d:
+        print('дорівнює', x + y)
+    elif '-' in d:
+        print('дорівнює', x - y)
+    elif '/' in d and y!=0:
+           print('дорівнює', x / y)
+    else:
+           print("некоректна дія")
+else:
+    print('not dilit na 0')
