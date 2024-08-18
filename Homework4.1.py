9834928716d30abcec6263e045e82123a989a6f1
User <aviator171513@gmail.com>
2024-08-18 14:08:59 +0300

    homework 4.1

--- Homework4.1.py
+++ Homework4.1.py
@@ -1,10 +1,9 @@
 list=[4,0,0,8,0,1,87,0,5,-3,0,-5,0,-56,6]
-new_list=[]
-zero_list=[]
 for i in list:
-    if i!=0:
-       new_list.append(i)
-    else:
-        zero_list.append(i)
-print(new_list+zero_list)
+    if i==0:
+        list.remove(i)
+        list.append(i)
+print(list)
+
+
 
