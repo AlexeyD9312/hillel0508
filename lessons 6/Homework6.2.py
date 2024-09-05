f62c17d404cbb75d823c26a0a6abab3ac1b6d566
User <aviator171513@gmail.com>
2024-09-05 08:35:18 +0300

    homework 6.2 var2

--- lessons 6/Homework6.2.py
+++ lessons 6/Homework6.2.py
@@ -1,10 +1,17 @@
 
 my_number = int(input('Enter number:'))
-if my_number > 0 and my_number < 8640000:
+if my_number >= 0 and my_number < 8640000:
   minutes, seconds = divmod(my_number, 60)
   hours, minutes = divmod(minutes, 60)
   days,hours = divmod(hours, 24)
-  print(days, 'День,')
+  if days >=5:
+      print(days, 'Днів,')
+  elif days == 0:
+      print(days, 'Днів,')
+  elif days == 1:
+      print(days, 'День,')
+  else:
+    print(days, 'Дня,')
   print(hours, minutes, seconds, sep=':')
 else:
    print('wrong range')
