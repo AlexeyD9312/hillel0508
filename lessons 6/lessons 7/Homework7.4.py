eb5ccf0989b4dd354039c4015ec295ba0126715e
User <aviator171513@gmail.com>
2024-09-05 20:25:25 +0300

    homework 7.4

---
+++ lessons 6/lessons 7/Homework7.4.py
@@ -0,0 +1,6 @@
+def common_elements():
+    num_lis1 = [i for i in range(0, 100) if i % 3 == 0]
+    num_lis2 =  [i for i in range(0, 100) if i % 5 == 0]
+    gen_lis = list(set(num_lis1) & set(num_lis2))
+    print(gen_lis)
+    assert common_elements() == {0, 75, 45, 15, 90, 60, 30}
