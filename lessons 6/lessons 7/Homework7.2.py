d7be5e92f55b172d2fcf1d7d0ff24e1ebf7d3651
User <aviator171513@gmail.com>
2024-09-05 10:31:35 +0300

    homework 7.2 var

--- lessons 6/lessons 7/Homework7.2.py
+++ lessons 6/lessons 7/Homework7.2.py
@@ -1,15 +1,25 @@
-def correct_sentence(text_1,text_2):
-     text_1 = text_1[0].upper() + text_1[1:]
-     if text_1[-1] != ".":
-         print(text_1 + ".")
-     else:
-         print(text_1)
-     text_2 = text_2[0].upper() + text_2[1:]
-     if text_2[-1] != ".":
-         print(text_2 + ".")
-     else:
-         print(text_2)
-correct_sentence(input("Print your text_1:"),input("Print your text_2:"))
+def correct_sentence(text_1, text_2):
+    if text_2 =="":
+        text_1 = text_1[0].upper() + text_1[1:]
+        if text_1[-1] != ".":
+            print(text_1+".")
+        else:
+                print(text_1)
+    elif text_1 =="":
+        text_2 = text_2[0].upper() + text_2[1:]
+        if text_2[-1] != ".":
+            print(text_2 + ".")
+        else:
+            print(text_2)
+    else:
+        text_1 = text_1[0].upper() + text_1[1:]
+
+        if text_2[-1] != ".":
+            print((text_1) + " " + (text_2 + "."))
+        else:
+            print(text_1 + " " + text_2)
+
+correct_sentence(input("Print your text_1:"), input("Print your text_2:"))
 assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
 assert correct_sentence("hello") == "Hello.", 'Test2'
 assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
