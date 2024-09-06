46818310d08099167e335e965c59b066cca38c67
User <aviator171513@gmail.com>
2024-09-06 22:17:48 +0300

    homework 8.2

--- lessons 8/Homework8.2.py
+++ lessons 8/Homework8.2.py
@@ -1,11 +1,13 @@
+text = input("Введіть текст:")
 def is_palindrome(text):
     text = ''.join([a for a in text if a.isalpha()])
     text = text.lower()
     rev_text = text[::-1]
-    print(text)
-    print(rev_text)
-    print("True" if rev_text == text else "False")
-text = input()
+    if rev_text == text:
+        print("True")
+    else:
+        print("False")
+
 #assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
 #assert is_palindrome('0P') == False, 'Test2'
 #assert is_palindrome('a.') == True, 'Test3'
