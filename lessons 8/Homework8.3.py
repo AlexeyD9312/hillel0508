269a820fd86e58b86b3cdf5c787450e002fa0732
User <aviator171513@gmail.com>
2024-09-07 15:47:17 +0300

    homework 8.3

--- lessons 8/Homework8.3.py
+++ lessons 8/Homework8.3.py
@@ -1,14 +1,10 @@
-
-some_list = [1, 2, 1, 1]
-
+import collections
 def find_unique_value(some_list):
-    unique_value = []
-    unique_numbers = set(some_list)
-    for some_list in unique_numbers:
-        unique_value.append(some_list)
-    return unique_value
+    print([a for a,  c in collections.Counter(some_list).items() if c == 1])
 
+some_list = [1,2,3,1,1]
 print(find_unique_value(some_list))
+
 assert find_unique_value([1, 2, 1, 1]) == 2, 'Test1'
 assert find_unique_value([2, 3, 3, 3, 5, 5]) == 2, 'Test2'
 assert find_unique_value([5, 5, 5, 2, 2, 0.5]) == 0.5, 'Test3'
