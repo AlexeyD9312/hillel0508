a9d3af6910554f153fc4a0397f1bafebe7f28c64
User <aviator171513@gmail.com>
2024-09-07 14:17:47 +0300

    homework 8.2

--- lessons 8/Homework8.2.py
+++ lessons 8/Homework8.2.py
@@ -8,8 +8,9 @@ def is_palindrome(text):
     else:
         print("False")
 
-#assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
-#assert is_palindrome('0P') == False, 'Test2'
-#assert is_palindrome('a.') == True, 'Test3'
-#assert is_palindrome('aurora') == False, 'Test4'
-#print("ОК")
+print(is_palindrome(text))
+assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
+assert is_palindrome('0P') == False, 'Test2'
+assert is_palindrome('a.') == True, 'Test3'
+assert is_palindrome('aurora') == False, 'Test4'
+print("ОК")
